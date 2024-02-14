fc5d670cfc35f6c42a2685cf665d0f3ba537410d
akborhossain <smakbor33@gmail.com>
2024-02-14 12:26:13 +0600

    custom paramiter

--- api2/api/mypaginations.py
+++ api2/api/mypaginations.py
@@ -4,3 +4,5 @@ from rest_framework.pagination import PageNumberPagination
 
 class MyPagination(PageNumberPagination):
     page_size=3
+    page_query_param='p'
+    page_size_query_param='records'
